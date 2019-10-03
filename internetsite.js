26b76db9bb6eb24484e37241cab3ebad286e0267
sidharth nijhawan <sidnijhawan>
2019-10-03 17:12:21 +0530

    heroku started

--- internetsite.js
+++ internetsite.js
@@ -191,7 +191,7 @@ var server = http.createServer(function (req, res)
     }
     res.end();
 });
-
-server.listen(3000, function () {
+var port = process.env.port||3000;
+server.listen(port, function () {
     console.log("server is at port 3000");
 })
